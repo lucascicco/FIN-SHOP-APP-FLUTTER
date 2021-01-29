fe60b4badb258b3a5e224a7ac17e7d0141d5db8d
lucacicco <lucasciccomy@gmail.com>
2021-01-29 06:43:56 -0300

    http_exception

---
+++ lib/providers/auth.dart
@@ -0,0 +1,6 @@
+import 'dart:convert';
+import 'dart:async';
+
+import 'package:flutter/widgets.dart';
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
