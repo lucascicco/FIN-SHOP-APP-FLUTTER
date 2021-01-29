1deea3637d3a36f9115d03c870210586dacb3767
lucacicco <lucasciccomy@gmail.com>
2021-01-29 11:27:52 -0300

    fixing the layout 2

--- lib/widgets/order_item.dart
+++ lib/widgets/order_item.dart
@@ -22,7 +22,7 @@ class _OrderItemState extends State<OrderItem> {
     return AnimatedContainer(
       duration: Duration(milliseconds: 300),
       height:
-          _expanded ? min(widget.order.products.length * 20.0 + 110, 200) : 95,
+          _expanded ? min(widget.order.products.length * 20.0 + 130, 200) : 95,
       child: Card(
         margin: EdgeInsets.all(10),
         child: Column(
@@ -45,7 +45,7 @@ class _OrderItemState extends State<OrderItem> {
               duration: Duration(milliseconds: 300),
               padding: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
               height: _expanded
-                  ? min(widget.order.products.length * 20.0 + 10, 100)
+                  ? min(widget.order.products.length * 20.0 + 30, 100)
                   : 0,
               child: ListView(
                 children: widget.order.products
@@ -57,6 +57,7 @@ class _OrderItemState extends State<OrderItem> {
                             flex: 2,
                             child: Text(
                               prod.title,
+                              overflow: TextOverflow.ellipsis,
                               style: TextStyle(
                                 fontSize: 18,
                                 fontWeight: FontWeight.bold,
